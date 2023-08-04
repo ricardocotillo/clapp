9c793cea62dbec93869f8209356eb61bcd852baa
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-03 22:34:41 -0500

    add model to admin

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import UserComment, ClubComment, MatchComment
+
+admin.site.register(UserComment)
+admin.site.register(ClubComment)
+admin.site.register(MatchComment)
--- match/admin.py
+++ match/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Match
+from .models import Match, MatchPlayer
 
 admin.site.register(Match)
+admin.site.register(MatchPlayer)

0d9d193e3c5141cbf1a320815eb569b97532e437
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-06 00:11:21 -0500

    refactor sport filter

--- club/filters.py
+++ club/filters.py
@@ -9,6 +9,16 @@ class MembershipFilter(filters.FilterSet):
 
 
 class ClubFilter(filters.FilterSet):
+    sport = filters.NumberFilter(
+        field_name='sport',
+        method='filter_sport',
+    )
+
     class Meta:
         model = Club
         fields = ('owner', 'members', 'sport')
+
+    def filter_sport(self, queryset, name, value):
+        sports = self.request.GET.getlist('sport')
+        sports = [int(s) for s in sports]
+        return queryset.filter(sport__in=sports)

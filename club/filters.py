c635188713b7a08e28e7b3b0c8a5262fb0db09c9
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-06 12:24:19 -0500

    create new filter field

--- club/filters.py
+++ club/filters.py
@@ -2,6 +2,10 @@ from django_filters import rest_framework as filters
 from .models import Membership, Club
 
 
+class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
+
+
 class MembershipFilter(filters.FilterSet):
     class Meta:
         model = Membership
@@ -9,16 +13,8 @@ class MembershipFilter(filters.FilterSet):
 
 
 class ClubFilter(filters.FilterSet):
-    sport = filters.NumberFilter(
-        field_name='sport',
-        method='filter_sport',
-    )
+    sport = NumberInFilter()
 
     class Meta:
         model = Club
         fields = ('owner', 'members', 'sport')
-
-    def filter_sport(self, queryset, name, value):
-        sports = self.request.GET.getlist('sport')
-        sports = [int(s) for s in sports]
-        return queryset.filter(sport__in=sports)

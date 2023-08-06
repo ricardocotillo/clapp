947eff86ffce47545daeb89e7dc13e1d6819e42d
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-05 20:46:20 -0500

    refactor serializer

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -25,28 +25,40 @@ class UserCommentSerializer(serializers.ModelSerializer):
 
 
 class MatchCommentSerializer(serializers.ModelSerializer):
-    sender = UserSerializer()
-    receiver = UserSerializer()
+    sender = UserSerializer(read_only=True)
+    sender_id = serializers.IntegerField()
     time_since_created = serializers.SerializerMethodField()
-    time_since_updated = serializers.SerializerMethodField()
 
     class Meta:
         model = MatchComment
-        fields = '__all__'
+        fields = (
+            'id',
+            'sender',
+            'receiver',
+            'sender_id',
+            'message',
+            'time_since_created',
+        )
 
     def get_time_since_created(self, obj: MatchComment):
         return timesince.timesince(obj.created_at).split(',')[0]
 
 
 class ClubCommentSerializer(serializers.ModelSerializer):
-    sender = UserSerializer()
-    receiver = UserSerializer()
+    sender = UserSerializer(read_only=True)
+    sender_id = serializers.IntegerField()
     time_since_created = serializers.SerializerMethodField()
-    time_since_updated = serializers.SerializerMethodField()
 
     class Meta:
         model = ClubComment
-        fields = '__all__'
+        fields = (
+            'id',
+            'sender',
+            'receiver',
+            'sender_id',
+            'message',
+            'time_since_created',
+        )
 
     def get_time_since_created(self, obj: ClubComment):
         return timesince.timesince(obj.created_at).split(',')[0]

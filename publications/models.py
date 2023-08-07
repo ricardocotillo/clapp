fa7b5e0991d501224c348aba5077ed1e61b8f4c6
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-07 10:28:43 -0500

    add reaction model

--- publications/models.py
+++ publications/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from authentication.models import User
+
+
+class ReactionType(models.IntegerChoices):
+    LIKE = 0, '❤️'
+
+
+class Reaction(models.Model):
+    type = models.IntegerField(choices=ReactionType.choices, null=True)
+    user = models.ForeignKey(
+        'authentication.User', on_delete=models.CASCADE, related_name='reactions')
+    publication = models.ForeignKey(
+        'Publication', on_delete=models.CASCADE, related_name='reactions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
 
 
 class Publication(models.Model):
@@ -15,3 +36,10 @@ class Publication(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user.email} - {self.created_at}'
+
+    def react(self, user: User, type: int):
+        return Reaction.objects.create(
+            type=type,
+            user=user,
+            publication=self,
+        )
